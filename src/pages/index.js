06407dd2c6bb163c5143f65e0d031e1968b8fe19
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2024-02-21 15:25:58 -0500

    React activities

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Artists from './qcomps/artistsRemoveArr'
+import ListObj from './qcomps/arrObj'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <ListObj />
     </div>
   )
 }
